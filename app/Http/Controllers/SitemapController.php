198a817b0cc99abe452c1c3ddc6768081e707a8e
ChAkhmadiyev <ch.akhmadi@gmail.com>
2018-11-19 16:00:39 +0600

    SiteMap

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -7,6 +7,7 @@ use App\Post;
 use App\Tour;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Vshapovalov\Pages\Page;
 
 class SitemapController extends Controller
 {
@@ -28,6 +29,9 @@ class SitemapController extends Controller
 
         //mk
         $tours = Tour::all();
+
+        // $pages = Page::all();
+        $pages = Page::where('use_in_sitemap', 1)->get();
 
         //online
         $excursions = Excursion::all();
@@ -57,6 +61,15 @@ class SitemapController extends Controller
         $items = $items->merge($excursions->map(function($item,$key){
             return [
 	            'loc' => page_route('excursion', ['slug' => $item->slug]),
+                'lastmod' => $item->updated_at->tz('GMT')->toAtomString(),
+                'changefreq' => 'monthly',
+                'priority' => 1
+            ];
+        }));
+
+        $items = $items->merge($pages->map(function($item,$key){
+            return [
+	            'loc' => page_route($item),
                 'lastmod' => $item->updated_at->tz('GMT')->toAtomString(),
                 'changefreq' => 'monthly',
                 'priority' => 1
