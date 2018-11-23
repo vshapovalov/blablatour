ccb6c60b6dfafcfb9f9fe6dca27f26300009b6f2
ChAkhmadiyev <ch.akhmadi@gmail.com>
2018-11-23 12:18:41 +0600

    Meta tags to pages

--- resources/views/default_page.blade.php
+++ resources/views/default_page.blade.php
@@ -1,4 +1,15 @@
+@php
+    $labels = cache()->rememberForever('labels', function() {
+        return \App\Label::all();
+    });
+@endphp
+
 @extends('layouts.master')
+
+@section('meta_title', $page->meta_title . ' | ' . $labels->firstWhere('code','=','site_title')->label)
+@section('meta_description', $page->meta_description)
+@section('meta_keywords', $page->meta_keywords)
+
 
 @section('page')
     <div id="colorlib-blog" class="default-view">
