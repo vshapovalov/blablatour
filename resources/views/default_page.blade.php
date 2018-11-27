cdc4d177c84ca789b874d19ec132209fa9fb12c3
ChAkhmadiyev <ch.akhmadi@gmail.com>
2018-11-27 14:20:33 +0600

    Meta title

--- resources/views/default_page.blade.php
+++ resources/views/default_page.blade.php
@@ -1,12 +1,6 @@
-@php
-    $labels = cache()->rememberForever('labels', function() {
-        return \App\Label::all();
-    });
-@endphp
-
 @extends('layouts.master')
 
-@section('meta_title', $page->meta_title . ' | ' . $labels->firstWhere('code','=','site_title')->label)
+@section('meta_title', $page->meta_title)
 @section('meta_description', $page->meta_description)
 @section('meta_keywords', $page->meta_keywords)
 
