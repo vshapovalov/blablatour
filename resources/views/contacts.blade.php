ac5acb3e35d4a81185e68066513843d4a3a183f2
akhmadi <ch.akhmadi@gmail.com>
2018-12-30 21:22:44 +0300

    Meta fields

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.master')
 
+@section('meta_title', $page->meta_title)
+@section('meta_description', $page->meta_description)
+@section('meta_keywords', $page->meta_keywords)
+
 @php
     $labels = cache()->rememberForever('labels', function() {
         return \App\Label::all();
